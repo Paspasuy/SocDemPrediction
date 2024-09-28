f0041fcc3c3551bf42b26c7be6c6cd32e249c1f6
Nikulin Pavel <p.a.nikulin@tinkoff.ru>
2024-09-28 19:43:02 +0300

    Puteshestvenniki

--- utils/extractors/user_extractor.py
+++ utils/extractors/user_extractor.py
@@ -19,7 +19,7 @@ class UserFeatureExtractor:
     Добавляет фичи
     - has_{os}: есть ли у человека устройство с такой OS
     - has_app_installed: есть ли мобильное приложение
-
+    - region_count: сколько регионов посещал
     """
 
     def fit(self):
@@ -27,21 +27,26 @@ class UserFeatureExtractor:
 
     def transform(self, events, features):
         devices = ['Android', 'Windows', 'Mac', 'iOS', 'iPadOS']
-        has_devices = events.groupby('viewer_uid').agg(
-            **{f'has_{device.lower()}': ('ua_os', _get_get_dev(device)) for device in devices}
+        # has_devices = events.groupby('viewer_uid').agg(
+        #     **{f'has_{device.lower()}': ('ua_os', _get_get_dev(device)) for device in devices}
+        # )
+        #
+        # has_other = events.groupby('viewer_uid').agg(
+        #     has_other = ('ua_os', lambda x: int(sum([x.value_counts().get(d, 0) for d in devices]) == 0))
+        # )
+        #
+        # has_app_installed = events.groupby('viewer_uid').agg(
+        #     has_app_installed = ('ua_client_type', lambda x: int(x.value_counts().get('mobile app', 0) > 0))
+        # )
+
+        travel_count = events.groupby('viewer_uid').agg(
+            travel_count = ('region', lambda x: int(x.nunique()))
         )
 
-        has_other = events.groupby('viewer_uid').agg(
-            has_other = ('ua_os', lambda x: int(sum([x.value_counts().get(d, 0) for d in devices]) == 0))
-        )
-
-        has_app_installed = events.groupby('viewer_uid').agg(
-            has_app_installed = ('ua_client_type', lambda x: int(x.value_counts().get('mobile app', 0) > 0))
-        )
-
-        features = pd.merge(has_devices, features, on='viewer_uid', how='inner')
-        features = pd.merge(has_other, features, on='viewer_uid', how='inner')
-        features = pd.merge(has_app_installed, features, on='viewer_uid', how='inner')
+        # features = pd.merge(has_devices, features, on='viewer_uid', how='inner')
+        # features = pd.merge(has_other, features, on='viewer_uid', how='inner')
+        # features = pd.merge(has_app_installed, features, on='viewer_uid', how='inner')
+        features = pd.merge(travel_count, features, on='viewer_uid', how='inner')
 
         return events, features
 
