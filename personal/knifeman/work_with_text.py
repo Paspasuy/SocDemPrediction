bc6debc91c0b3473e45909e77d187b6bb0da7481
Dmitrii Kuzakov <kuzakov-dn@qavm-efb872e7.qemu>
2024-09-29 06:54:54 +0300

    update work with text

--- personal/knifeman/work_with_text.py
+++ personal/knifeman/work_with_text.py
@@ -1,20 +1,54 @@
-import torch
 import os
-import torch.nn.functional as F
+import torch
 from torch import Tensor
 import torch.nn as nn
 from transformers import AutoTokenizer, AutoModel
 import pandas as pd
-import seaborn as sns
 import numpy as np
 from tqdm import tqdm, trange
+from sklearn.decomposition import PCA
 
 
-def download_embeddings(video, device_ids: list, model_hard=False):
+def last_token_pool(last_hidden_states: Tensor,
+                    attention_mask: Tensor) -> Tensor:
+    left_padding = (attention_mask[:, -1].sum() == attention_mask.shape[0])
+    if left_padding:
+        return last_hidden_states[:, -1]
+    else:
+        sequence_lengths = attention_mask.sum(dim=1) - 1
+        batch_size = last_hidden_states.shape[0]
+        return last_hidden_states[torch.arange(batch_size, device=last_hidden_states.device), sequence_lengths]
+    
+
+def reduce_dimensionality(data, target_dimension):
     """
-    Функция для загрузки эмбеддингов. Есть тяжёлая 1.5B модель, есть лёгкая 560M
+    Уменьшает размерность данных с d до target_dimension с помощью PCA.
+
+    Args:
+        data (numpy.ndarray): Исходные данные размерности (n_samples, d).
+        target_dimension (int): Желаемая размерность выходных данных.
+
+    Returns:
+        numpy.ndarray: Данные размерности (n_samples, target_dimension).
     """
+    # Инициализируем PCA с целевой размерностью
+    pca = PCA(n_components=target_dimension)
+    
+    # Применяем PCA к данным
+    reduced_data = pca.fit_transform(data)
+    
+    return reduced_data
+
+
+def download_embeddings(video, device_ids: list, embeds_size=32, model_hard=False) -> np.array:
+    """
+    Функция для загрузки эмбеддингов. Есть тяжёлая 1.5B модель, есть лёгкая 560M.
+    Проходимся по текстам и достаём эмбеддинги из них моделькой, а затем с помощью
+    k_means сжимаем эмбеддинги до размерности embeds_size
+    """
+
     os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+    video = video.copy()
 
     # Для инструкционных эмбеддингов
     def get_detailed_instruct(task_description: str, query: str) -> str:
@@ -42,16 +76,6 @@ def download_embeddings(video, device_ids: list, model_hard=False):
 
     max_length = 128  # Максимальная длина токенов в тексте
 
-    def last_token_pool(last_hidden_states: Tensor,
-                        attention_mask: Tensor) -> Tensor:
-        left_padding = (attention_mask[:, -1].sum() == attention_mask.shape[0])
-        if left_padding:
-            return last_hidden_states[:, -1]
-        else:
-            sequence_lengths = attention_mask.sum(dim=1) - 1
-            batch_size = last_hidden_states.shape[0]
-            return last_hidden_states[torch.arange(batch_size, device=last_hidden_states.device), sequence_lengths]
-    
     def tokenize(texts):
         tensors = tokenizer(texts, max_length=max_length, padding=True, truncation=True, return_tensors="pt")
         return {k : v for k, v in tensors.items()}
@@ -76,8 +100,10 @@ def download_embeddings(video, device_ids: list, model_hard=False):
         video['text'] = video.apply(lambda x: 'Название видео: "' + x['title'] + '"; Категория видео: ' + x['category'], axis=1)
         video['query'] = video['text'].apply(lambda x: get_detailed_instruct("Определи средний возраст целевой аудитории данного видео", x))
         embeds_large = embed_batched(list(video['query']))
-        return embeds_large
+        embeds_small = reduce_dimensionality(embeds_large, embeds_size)
+        return embeds_small
     
     video['text'] = video.apply(lambda x: x['title'] + '; Категория: ' + x['category'], axis=1)
-    embeds_large = embed_batched(list(video['text']))
-    return embeds_large
+    embeds = embed_batched(list(video['text']))
+    embeds_small = reduce_dimensionality(embeds, embeds_size)
+    return embeds_small
